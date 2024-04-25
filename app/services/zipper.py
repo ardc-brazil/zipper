7874c87e40fd956b9322e08ae4af5b94fd2138c8
Caio Maia <caaiomaia@gmail.com>
2024-04-24 23:30:17 -0300

    Test zipper service

--- app/services/zipper.py
+++ app/services/zipper.py
@@ -3,30 +3,31 @@ import os
 import tempfile
 import zipfile
 from minio import Minio
+import uuid
 
 from app.models.zipper import ZippedResource
 
 
 class ZipperService:
-    def __init__(self, minio_host, minio_access_key, minio_secret_key, temp_dir):
-        self._minio_client = self._create_minio_client(
-            minio_host, minio_access_key, minio_secret_key
-        )
+    def __init__(self, minio_client: Minio, temp_dir: str):
+        self._minio_client = minio_client
         self._temp_dir = temp_dir
-        pass
-
-    def _create_minio_client(self, host, access_key, secret_key):
-        return Minio(host, access_key=access_key, secret_key=secret_key, secure=False)
 
     def zip_files(
-        self, bucket: str, file_names: list[str], zip_name: str
+        self, bucket: str, file_names: list[str], zip_name: str | None = None
     ) -> ZippedResource:
+        if not zip_name:
+            zip_name = f"{uuid.uuid4()}.zip"
+
+        if ".zip" not in zip_name:
+            zip_name = f"{zip_name}.zip"
+
         temp_zip_file = tempfile.NamedTemporaryFile(
-            prefix=zip_name, dir=self._temp_dir, delete=False
+            dir=self._temp_dir, delete=False
         )
 
         try:
-            with zipfile.ZipFile(temp_zip_file, "w") as zipf:
+            with zipfile.ZipFile(temp_zip_file, "w", zipfile.ZIP_DEFLATED) as zipf:
                 for file in file_names:
                     with self._minio_client.get_object(bucket, file) as obj:
                         zipf.writestr(os.path.basename(file), obj.read())
@@ -36,6 +37,7 @@ class ZipperService:
             logging.error(f"Failed to zip files: {e}")
             return ZippedResource(success=False)
         finally:
-            os.remove(temp_zip_file)
+            logging.info(f"Removing temporary zip file: {temp_zip_file.name}")
+            os.remove(temp_zip_file.name)
 
         return ZippedResource(success=True, bucket=bucket, name=zip_name)
